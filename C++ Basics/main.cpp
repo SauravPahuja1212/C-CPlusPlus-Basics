a19bbcc62f5f867c8c33405ed6c4c2352558b20c
SauravPahuja <sauravpahuja1995@gmail.com>
2024-05-02 22:46:27 +0530

    Initial Commit

---
+++ C++ Basics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Basics
+//
+//  Created by Saurav Pahuja on 02/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
